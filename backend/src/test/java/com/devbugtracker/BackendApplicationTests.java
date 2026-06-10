34fc504b10637e702890408927a1099eda9a3d54
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-06-10 10:16:01 -0300

    chore: criar projeto inicial do backend

---
+++ backend/src/test/java/com/devbugtracker/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
