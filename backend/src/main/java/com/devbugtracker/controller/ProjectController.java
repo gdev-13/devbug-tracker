447fcafc4cc83e91f4e373a768bb437584fe687a
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-06-10 15:34:31 -0300

    feat: adicionar endpoint de criação de projeto

--- backend/src/main/java/com/devbugtracker/controller/ProjectController.java
+++ backend/src/main/java/com/devbugtracker/controller/ProjectController.java
@@ -2,13 +2,19 @@ package com.devbugtracker.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devbugtracker.dto.ProjectRequestDTO;
 import com.devbugtracker.dto.ProjectResponseDTO;
 import com.devbugtracker.service.ProjectService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,5 +27,11 @@ public class ProjectController {
 	@GetMapping
     public List<ProjectResponseDTO> findAll() {
         return projectService.findAll();
+    }
+	
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProjectResponseDTO create(@RequestBody @Valid ProjectRequestDTO requestDTO) {
+        return projectService.create(requestDTO);
     }
 }
