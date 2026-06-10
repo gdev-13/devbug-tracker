bd8636f652c2e6b831d1cc47e7f4ad4afe82c8bc
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-06-10 15:10:39 -0300

    feat: implementar listagem de projetos

--- backend/src/main/java/com/devbugtracker/service/ProjectService.java
+++ backend/src/main/java/com/devbugtracker/service/ProjectService.java
@@ -1,7 +1,11 @@
 package com.devbugtracker.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.devbugtracker.dto.ProjectResponseDTO;
+import com.devbugtracker.entity.Project;
 import com.devbugtracker.repository.ProjectRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +15,23 @@ import lombok.RequiredArgsConstructor;
 public class ProjectService {
 
 	private final ProjectRepository projectRepository;
+	
+    public List<ProjectResponseDTO> findAll() {
+        return projectRepository.findAll()
+                .stream()
+                .map(this::toResponseDTO)
+                .toList();
+    }
+
+    private ProjectResponseDTO toResponseDTO(Project project) {
+        return new ProjectResponseDTO(
+                project.getId(),
+                project.getName(),
+                project.getDescription(),
+                project.getTechnologies(),
+                project.getStatus(),
+                project.getCreatedAt(),
+                project.getUpdatedAt()
+        );
+    }
 }
