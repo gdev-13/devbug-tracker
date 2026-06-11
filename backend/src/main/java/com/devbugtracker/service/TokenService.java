d68a3dbedd33273519f10692e890a434f0066fcb
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-06-11 13:00:48 -0300

    feat: adicionar serviço de geração de token

---
+++ backend/src/main/java/com/devbugtracker/service/TokenService.java
@@ -0,0 +1,34 @@
+package com.devbugtracker.service;
+
+import java.time.Instant;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.devbugtracker.entity.AppUser;
+
+@Service
+public class TokenService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(AppUser appUser) {
+        Instant now = Instant.now();
+        Instant expiresAt = now.plusMillis(expiration);
+
+        return JWT.create()
+                .withSubject(appUser.getEmail())
+                .withClaim("id", appUser.getId())
+                .withClaim("name", appUser.getName())
+                .withIssuedAt(Date.from(now))
+                .withExpiresAt(Date.from(expiresAt))
+                .sign(Algorithm.HMAC256(secret));
+    }
+}
